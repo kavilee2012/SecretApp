75232bb45d99ffbb251e55cbbd92ac06a4962a8b
dczj <kavilee@163.com>
2016-05-12 18:27:25 +0800

    5.12

--- app/src/main/java/com/lz/www/secretapp/atys/TimeLineActivity.java
+++ app/src/main/java/com/lz/www/secretapp/atys/TimeLineActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 
+import com.lz.www.secretapp.Config;
 import com.lz.www.secretapp.R;
+import com.lz.www.secretapp.data.MyContact;
+import com.lz.www.secretapp.net.UploadContract;
 
 /**
  * Created by Administrator on 2016-05-10.
@@ -15,5 +18,20 @@ public class TimeLineActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timeline_layout);
+
+        String phoneNum =getIntent().getStringExtra(Config.KEY_PHONE_NUM);
+        String token=getIntent().getStringExtra(Config.KEY_TOKEN);
+//        String phoneMD5=
+        new UploadContract(phoneNum, token, MyContact.getContractJsonString(this), new UploadContract.SuccessCallback() {
+            @Override
+            public void onSuccess(String result) {
+
+            }
+        }, new UploadContract.FailCallback() {
+            @Override
+            public void onFail() {
+
+            }
+        });
     }
 }
